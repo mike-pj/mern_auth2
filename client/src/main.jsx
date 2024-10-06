00b90e057aceeae5cc23b87c82516e447bc15330
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2024-10-06 23:40:06 +0100

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 // import from store.js which is in the redux folder
-import { store } from './redux/store.js'
+import { store, persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store = {store}>
-    <App />
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>,
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
+  // createRoot(document.getElementById('root')).render(
+  //   <StrictMode>
+  //     <App />
+  //   </StrictMode>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
-import useReducer  from './user/userSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 
 export const store = configureStore({
-    reducer: {user: useReducer},
+    // reducer: {user: userReducer},
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }),
-});
 
-// from here we go to main.jsx and import
+    // from here we go to main.jsx and import
 // 1.) store from redux folder
 // 2.) provider from react-redux
+});
+
+
+export const persistor = persistStore(store);
+// from here we go to main.jsx and import
+// persist as persistGate
+
